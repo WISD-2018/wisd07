cfca5522b5f075da295d0d39d809947ce2254d10
3A417012 <nds10745@gmail.com>
2018-12-10 20:23:03 +0800

    新增aa.a1和aa.a2功能

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,13 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/aa/a1', function () {
+    return view('aa.a1');
+});
+Route::get('/aa/a2', function () {
+    return view('aa.a2');
+});
+
 Route::get('/bb/b1', function () {
     return view('bb.b1');
 });
