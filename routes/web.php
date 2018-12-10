330e25734e8917ebd94fadbbf1599129111a3bbf
3A417012 <nds10745@gmail.com>
2018-12-10 23:37:40 +0800

    新增cc.c1和cc.2功能

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,10 @@ Route::get('/bb/b1', function () {
 Route::get('/bb/b2', function () {
     return view('bb.b2');
 });
+
+Route::get('/cc/c1', function () {
+    return view('cc.c1');
+});
+Route::get('/cc/c2', function () {
+    return view('cc.c2');
+});
